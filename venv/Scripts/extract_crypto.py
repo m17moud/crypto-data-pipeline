7c117d6c71d195661205ceac6bffc68b228a04c3
m17moud <97606241+m17moud@users.noreply.github.com>
2026-03-09 00:47:55 +0200

    first pipeline step

---
+++ venv/Scripts/extract_crypto.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+url = "https://api.coingecko.com/api/v3/coins/markets"
+
+params = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 10,
+    "page": 1
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+df = pd.DataFrame(data)
+
+print(df.head())
