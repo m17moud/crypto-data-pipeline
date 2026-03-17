0e54163de569b0db51ee643a2975e9868cd9bec5
m17moud <97606241+m17moud@users.noreply.github.com>
2026-03-17 13:39:40 +0200

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import logging
 from scripts.extract_crypto import extract_data
 from scripts.load_to_db import load_data
 
+logging.basicConfig(
+    filename="pipeline.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def run_pipeline():
+    
+    logging.info("Pipeline started")
 
     data = extract_data()
 
     load_data(data)
 
-    print("Pipeline executed successfully")
+    logging.info("Pipeline finished successfully")
 
 
 if __name__ == "__main__":
