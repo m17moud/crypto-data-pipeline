3b487d3f40e56845df80351f1b0f2dacd36d560a
m17moud <97606241+m17moud@users.noreply.github.com>
2026-03-17 13:41:03 +0200

    load_to_db.py

--- scripts/load_to_db.py
+++ scripts/load_to_db.py
@@ -1,15 +1,25 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 def load_data(data):
 
     conn = psycopg2.connect(
-        host="localhost",
-        database="crypto_db",
-        user="postgres",
-        password="YOUR_PASSWORD"
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
+
+
 
     cur = conn.cursor()
 
@@ -18,6 +28,12 @@ def load_data(data):
             INSERT INTO crypto_prices
             (coin_name, symbol, price_usd, market_cap, timestamp)
             VALUES (%s,%s,%s,%s,%s)
+                ON CONFLICT (symbol)
+                DO UPDATE SET
+                price_usd = EXCLUDED.price_usd,
+                market_cap = EXCLUDED.market_cap,
+                timestamp = EXCLUDED.timestamp
+
         """, (
             coin["name"],
             coin["symbol"],
